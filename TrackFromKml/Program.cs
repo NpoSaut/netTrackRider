c587d49661dec42d92a1bc2992e2a4c5528baef0
Evgeniy Plyusnin <eninbox@gmail.com>
2013-11-25 16:41:16 +0600

    Файл решения сконвертирован непонятно как и зачем. Добавлена программа для генерации треков

---
+++ TrackFromKml/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace TrackFromKml
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputDocument = XDocument.Load(args[0]);
+            var inputString =
+                inputDocument.Root
+                         .Element("Document")
+                         .Elements("Placemark").First()
+                         .Elements("LineString").First()
+                         .Element("coordinates").Value;
+            var outputString = inputString.Replace(' ', '\n').Replace(',', ' ');
+            using (TextWriter tw = new StreamWriter(args[1], false))
+            {
+                tw.Write(outputString);
+            }
+            Console.WriteLine("Готово");
+        }
+    }
+}
